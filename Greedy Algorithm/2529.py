5874d58047d79d72d9263746613a866748fb8e93
Junseok5 <vkehrkrj@naver.com>
2019-07-05 17:25:36 +0900

    2529 solved

--- Greedy Algorithm/2529.py
+++ Greedy Algorithm/2529.py
@@ -1,9 +1,32 @@
-k = int(input())
-ineq = list(map(str, input().split()))
+K = int(input())
+INEQUS = list(map(str, input().split()))
 
-min_nums = []
-max_nums = []
+visited = [False for _ in range(10)]
+result_list = []
 
-"""
-    0 < 2 > 1 >
-"""
+
+def dfs(v, cnt, result):
+    if cnt == K:
+        result_list.append(result)
+    else:
+        for i in range(10):
+            if not visited[i]:
+                if INEQUS[cnt] == "<":
+                    if v >= i:
+                        continue
+                else:
+                    if v <= i:
+                        continue
+
+                visited[i] = True
+                dfs(i, cnt + 1, result + str(i))
+
+    visited[v] = False
+
+
+for i in range(10):
+    visited[i] = True
+    dfs(i, 0, str(i))
+
+print(result_list[len(result_list) - 1])
+print(result_list[0])
